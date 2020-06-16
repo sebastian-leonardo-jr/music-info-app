12a90f1ac854602f42ab4dcc0924aa0ade011e13
Sebastian Adamo <seb.adamo10@gmail.com>
2020-06-16 22:46:08 +0100

    Initialising

---
+++ src/main/java/com/sebastianjr124/music/MainApplication.java
@@ -0,0 +1,13 @@
+package com.sebastianjr124.music;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainApplication.class, args);
+	}
+	
+}
---
+++ src/test/java/com/sebastianjr124/music/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sebastianjr124.music;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
